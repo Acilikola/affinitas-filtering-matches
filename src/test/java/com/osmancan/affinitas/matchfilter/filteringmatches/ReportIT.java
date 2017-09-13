80e31d92b4aff9c96f6323987974b248cfb4ac89
acilikola <ocornek@hotmail.com>
2017-09-13 15:03:27 +0300

    TDD - new test(check repo get all matches link + ensure returned list has elements)

--- src/test/java/com/osmancan/affinitas/matchfilter/filteringmatches/ReportIT.java
+++ src/test/java/com/osmancan/affinitas/matchfilter/filteringmatches/ReportIT.java
@@ -3,6 +3,8 @@ package com.osmancan.affinitas.matchfilter.filteringmatches;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
+
 
 //Maven Failsafe Integration Test for Report Page
 public class ReportIT {
@@ -10,5 +12,10 @@ public class ReportIT {
 	@Test
 	public void reportAliveTest() {
 		RestAssured.get("/report").then().assertThat().statusCode(200);
+	}
+	
+	@Test
+	public void fetchMatchesTest() {
+		RestAssured.get("/report/matches").then().assertThat().statusCode(200).and().assertThat().body("size()", greaterThan(0));
 	}
 }
